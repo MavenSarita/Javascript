444e488c04b4c87e1eb02279a73a7fb3ff9056e8
MavenSarita <91108718+MavenSarita@users.noreply.github.com>
2023-11-09 09:43:12 +0000

    datatypes and variables

---
+++ .devcontainer/javascripttest/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 15255
+let accountEmail = "sarita@gmail.com"
+let accountPassword = "123456"
+let accountCity = "Bihar"
+let accountState;
+
+// accountId = 2 / not allowed
+accountEmail = "sarita@gmail.com"
+accountPassword = "123695"
+accountPassword = "258963"
+accountCity = "bettiah"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+console.log("accountId");
---
+++ .devcontainer/javascripttest/datatypes.js
@@ -0,0 +1,7 @@
+let name = "sarita"
+let age = 25
+let login = false
+let password = null // standalone value like object
+let temprature ;
+
+console.log(typeof null)
